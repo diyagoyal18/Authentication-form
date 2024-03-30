4333eabc42db54bf339afdb52aae33f321836bba
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-03-30 16:06:12 +0530

    completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './Login';
+import Home from './Home';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
   <div>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/register' element={<Signup/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
 
+    </Routes>
+   </BrowserRouter>
   </div>
   )
 }
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,35 +1,28 @@
-import {useState} from 'react';
+import axios from 'axios';
+import {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios'
-function Signup () {
-    const [name, setName] = useState();
+
+function Login() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const navigate = useNavigate()
     const handleSubmit= (e)=>{
         e.preventDefault()
-        axios.post('http://localhost:3001/register', {name, email, password})
+        axios.post('http://localhost:3001/login', { email, password})
         .then(result=>{console.log(result)
-        navigate('/login')
+            if(result.data=== "success" ){
+
+                navigate('/home')
+            }
         })
         .catch(err=> console.log(err))
     }
   return (
     <div className='d-flex justify-content-center align-items-center bg-secondart vh-100'>
       <div className='bg-white p-3 rounded w-25'>
-        <h2>Register</h2>
+        <h2>Login</h2>
         <form onSubmit={handleSubmit}>
-            <div className='mb-3'>
-                <label htmlFor="email">
-                    <strong>Name</strong>
-                </label>
-                <input type="text"
-                placeholder='Enter Name'
-                autoComplete='off'
-                name='email'
-                className='form-control rounded-0' 
-                onChange={(e)=>setName(e.target.value)}/>
-            </div>
+           
             <div className='mb-3'>
                 <label htmlFor="email">
                     <strong>Email</strong>
@@ -52,13 +45,13 @@ function Signup () {
                 className='form-control rounded-0'
                 onChange={(e)=>setPassword(e.target.value)} />
             </div>
-            <button type='submit' className='btn btn-success w-100 rounded-0'>Register</button>
-            <p>Already Have an Account</p>
-            <Link to="/login" className='btn btn-default border w-100 rounded-0 text-decoration-none'>Login</Link>
+            <button type='submit' className='btn btn-success w-100 rounded-0'>Login</button>
+            <p>Dont Have an Account</p>
+            <Link to="/register" className='btn btn-default border w-100 rounded-0 text-decoration-none'>Register</Link>
         </form>
       </div>
     </div>
   )
 }
 
-export default Signup
+export default Login
